65a2499d37d20886cdfb98387f19df1034d5206e
prutskov <lehaprutskov@gmail.com>
2019-02-24 13:36:40 +0300

    Initialize new project

---
+++ Tomogram.h
@@ -0,0 +1,32 @@
+﻿
+// Tomogram.h: главный файл заголовка для приложения PROJECT_NAME
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "включить stdafx.h до включения этого файла в PCH"
+#endif
+
+#include "resource.h"		// основные символы
+
+
+// CTomogramApp:
+// Сведения о реализации этого класса: Tomogram.cpp
+//
+
+class CTomogramApp : public CWinApp
+{
+public:
+	CTomogramApp();
+
+// Переопределение
+public:
+	virtual BOOL InitInstance();
+
+// Реализация
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTomogramApp theApp;
